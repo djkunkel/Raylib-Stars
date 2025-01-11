9ba5a8a335e932b82a831c3c66edcd624c50e348
djkunkel <djkunkel@gmail.com>
2025-01-11 00:21:19 -0600

    Drop some letters, cause why not

--- Starfield/Ship.cs
+++ Starfield/Ship.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -13,6 +14,7 @@ namespace Starfield
     {
 
         Vector2[] projectiles;
+        Vector2[] letters;
 
         Vector2 position;
         private int _width;
@@ -24,20 +26,27 @@ namespace Starfield
 
         int shotsFired = 0;
 
+        Stopwatch letterTimer = new Stopwatch();
+
         public Ship()
         {
             projectiles = new Vector2[20];
+            letters = new Vector2[50];
+
         }
 
         public override void Init(int width, int height)
         {
-            position = new Vector2(width/2, height-75);
+            position = new Vector2(width / 2, height - 75);
             _width = width;
             _height = height;
+            letterTimer.Start();
         }
 
         public override void Update(float frameTime)
         {
+
+            //movement
             if (IsKeyDown(KeyboardKey.Left))
             {
                 position.X = Math.Max(shipSize / 2, position.X - shipSpeed *frameTime);
@@ -48,6 +57,7 @@ namespace Starfield
             }
 
 
+            //update projectiles
             bool fired = false;
             for (int i = 0; i < projectiles.Length; i++)
             {
@@ -65,7 +75,38 @@ namespace Starfield
                 }
             }
 
+            //move all letters
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i].Y > 0)
+                {
+                    letters[i].Y = letters[i].Y + shipSize * frameTime;
+                }
+                else
+                {
+                    if (letters[i].Y > _height)
+                        letters[i].Y = 0;
+                }
+            }
+
+            //throw a letter out?
+            if (letterTimer.ElapsedMilliseconds > 3000)
+            {
+                letterTimer.Restart();
+                //find the first 0 letter that isn't being used right now
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i].Y == 0)
+                    {
+                        letters[i].Y = 1;
+                        letters[i].X = GetRandomValue(0, _width);
+                        break;
+                    }
+                }
+            }
+
             
+
         }
 
         public override void Draw()
@@ -79,9 +120,17 @@ namespace Starfield
 
             for (int i = 0; i < projectiles.Length; i++)
             {
-                if (projectiles[i].Y >= 0)
+                if (projectiles[i].Y > 0)
                 {
                     DrawCircle((int)projectiles[i].X, (int)projectiles[i].Y, 4.0f, Color.Green);
+                }
+            }
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i].Y > 0)
+                {
+                    DrawText("A", (int)letters[i].X, (int)letters[i].Y, 40, Color.Gold);
                 }
             }
 
